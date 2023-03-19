173b9a40b9772d58151ba91ad00a8ac23d5de4b7
winnux82 <vandermeulen.christophe@gmail.com>
2023-03-19 15:45:09 +0100

    :sparkles: PrimeNg Menu Bar :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoaderComponent } from './main-page/loader/loader.component';
-
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [AppComponent, MainPageComponent, LoaderComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MenubarModule,
     AppRoutingModule,
-    MatMenuModule,
-    // MenubarModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
