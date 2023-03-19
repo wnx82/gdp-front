9626e7ebc63c4c9e251bb6c2a3077058e3c736ea
winnux82 <vandermeulen.christophe@gmail.com>
2023-03-19 15:37:42 +0100

    side doesnt work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,32 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoaderComponent } from './main-page/loader/loader.component';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
 @NgModule({
   declarations: [AppComponent, MainPageComponent, LoaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatMenuModule,
+    // MenubarModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonToggleModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -4,30 +4,8 @@ import { CommonModule } from '@angular/common';
 // import { RouterModule, Routes } from '@angular/router';
 // import { MainPageComponent } from './main-page.component';
 // import { MenubarModule } from 'primeng/menubar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
-  imports: [
-    CommonModule,
-
-    MatMenuModule,
-    // MenubarModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonToggleModule,
-  ],
+  imports: [CommonModule],
 })
 export class MainPageModule {}
