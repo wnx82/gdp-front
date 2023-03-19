2e68ca5682f273b70bc08f5f58bf26ab189afbb8
winnux82 <vandermeulen.christophe@gmail.com>
2023-03-19 16:57:21 +0100

    :sparkles: feat/footer :sparkles:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+//app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenubarModule } from 'primeng/menubar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoaderComponent } from './main-page/loader/loader.component';
+import { FooterComponent } from './main-page/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MainPageComponent, LoaderComponent],
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    LoaderComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MenubarModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
