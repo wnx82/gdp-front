5567a3c9a3d3f81b31f98c04c5a548771cc65a75
winnux82 <vandermeulen.christophe@gmail.com>
2023-03-20 22:42:34 +0100

    :sparkles: feat/sidenav :sparkles:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { DashboardComponent } from './main-page/dashboard/dashboard.component';
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./main-page/main-page.module').then((m) => m.MainPageModule),
-  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+
   {
     path: 'agents',
     loadChildren: () =>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,22 @@
 //app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MenubarModule } from 'primeng/menubar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoaderComponent } from './main-page/loader/loader.component';
 import { FooterComponent } from './main-page/footer/footer.component';
+import { BodyComponent } from './main-page/body/body.component';
+import { SidenavComponent } from './main-page/sidenav/sidenav.component';
+import { SublevelMenuComponent } from './main-page/sidenav/sublevel-menu.component';
+import { DashboardComponent } from './main-page/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -34,27 +24,22 @@ import { FooterComponent } from './main-page/footer/footer.component';
     MainPageComponent,
     LoaderComponent,
     FooterComponent,
+    BodyComponent,
+    DashboardComponent,
+    SidenavComponent,
+    SublevelMenuComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     CommonModule,
-    MenubarModule,
     AppRoutingModule,
     HttpClientModule,
     LayoutModule,
     ReactiveFormsModule,
     FormlyModule.forRoot(),
     FormlyBootstrapModule,
-    MatToolbarModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
