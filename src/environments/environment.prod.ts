a0e04dc4bd6ece815eb40aaa58cc725f4f8215da
Vandermeulen Christophe <vandermeulen.christophe@gmail.com>
2024-06-02 12:26:08 +0200

    Committing changes before updating Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3098',
+    apiUrl: 'http://localhost:3003',
 };
 
 /*
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: `http://localhost:3098`,
+    apiUrl: `http://localhost:3003`,
 };
 
 /*
