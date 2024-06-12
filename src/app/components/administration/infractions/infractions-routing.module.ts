afbbd33d1a465b2fa3040faa3f6f41fed5a3e0b7
Vandermeulen Christophe <vandermeulen.christophe@gmail.com>
2024-06-12 16:26:51 +0200

    :zap: infractions :zap:

--- src/app/components/administration/infractions/infractions-routing.module.ts
+++ src/app/components/administration/infractions/infractions-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateInfractionComponent } from './create-infraction/create-infraction.component';
+import { EditInfractionComponent } from './edit-infraction/edit-infraction.component';
 import { InfractionsComponent } from './infractions.component';
 
-const routes: Routes = [{ path: '', component: InfractionsComponent }];
+const routes: Routes = [
+  { path: 'create', component: CreateInfractionComponent },
+  { path: ':id', component: EditInfractionComponent },
+  { path: '', component: InfractionsComponent },
+  // Add other routes
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/administration/infractions/infractions.module.ts
+++ src/app/components/administration/infractions/infractions.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { InfractionsRoutingModule } from './infractions-routing.module';
 import { InfractionsComponent } from './infractions.component';
+
 import { SharedUiModule } from '../../../services/shared-ui/shared-ui.module';
+import { CreateInfractionComponent } from './create-infraction/create-infraction.component';
+import { EditInfractionComponent } from './edit-infraction/edit-infraction.component';
 
 @NgModule({
-    declarations: [InfractionsComponent],
+    declarations: [InfractionsComponent, CreateInfractionComponent, EditInfractionComponent],
     imports: [CommonModule, InfractionsRoutingModule, SharedUiModule],
 })
 export class InfractionsModule {}
